ac749258e5951c0ee6f158596d93ae8590df6213
lukluk santoso <lukluk.santoso@bankmas.co.id>
2024-12-17 17:48:19 +0700

    update

--- handler/handler.go
+++ handler/handler.go
@@ -1,8 +1,9 @@
 package handler
 
-import (	
-	"fmt"	
+import (
+	"fmt"
 	"net/http"
+	"os/exec"
 	"strings"
 
 	redis "github.com/redis/go-redis/v9"
@@ -25,6 +26,11 @@ func (h *Handler) DefaultHandler() func(ctx *fasthttp.RequestCtx) {
 		if (string(ctx.Request.URI().Path()) == "/sidra/healthcheck") {
 			ctx.Response.Header.Set("Content-Type", "application/json")
 			ctx.Response.Header.Set("Server", "Sidra")
+			
+			if !processExists("sidra-data-plane") {
+				ctx.Response.SetStatusCode(http.StatusInternalServerError)
+				return
+			}
 			ctx.Response.SetStatusCode(http.StatusOK)
 			ctx.Response.SetBodyString("{\"status\":\"OK\"}")
 			return
@@ -117,4 +123,17 @@ func (h *Handler) DefaultHandler() func(ctx *fasthttp.RequestCtx) {
 		fmt.Println("Response: ", string(resp.Body()))
 		fasthttp.ReleaseResponse(resp)	
 	}
+}
+
+func processExists(processName string) bool {
+	cmd := exec.Command("pgrep", "-x", processName)
+	output, err := cmd.Output()
+
+	// If `pgrep` finds the process, it returns its PID(s); otherwise, it returns an error.
+	if err != nil {
+		return false
+	}
+
+	// Check if the output is not empty
+	return strings.TrimSpace(string(output)) != ""
 }
