883f1b5b094546f4f6d7d046b38666bd2465e242
lukluk <luklukaha@gmail.com>
2025-01-09 10:50:24 +0700

    add max mesagge size

--- handler/plugin.go
+++ handler/plugin.go
@@ -1,14 +1,19 @@
 package handler
 
 import (
-	"encoding/json"	
+	"encoding/json"
+	"log"
 	"net"
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/sidra-api/sidra/dto"
 )
 
 func (h *Handler) GoPlugin(pluginName string, request dto.SidraRequest) (response dto.SidraResponse) {	
+	//@TODO: for now body ignored
+	request.Body = ""
 	conn, err := net.Dial("unix", "/tmp/"+pluginName+".sock")
 	if err != nil {
 		return dto.SidraResponse{
@@ -17,10 +22,15 @@ func (h *Handler) GoPlugin(pluginName string, request dto.SidraRequest) (respons
 		}
 	}
 	defer conn.Close()
-
+	maxMessageSize := 16384
+	if size, err := strconv.Atoi(os.Getenv("MAX_MESSAGE_SIZE")); err == nil {
+		maxMessageSize = size
+	} else {
+		log.Printf("Invalid MAX_MESSAGE_SIZE value, using default: %v", err)
+	}	
 	requestBytes, _ := json.Marshal(request)
 	conn.Write(requestBytes)
-	buffer := make([]byte, 602768)
+	buffer := make([]byte, maxMessageSize)
 	n, _ := conn.Read(buffer[0:])
 	json.Unmarshal(buffer[:n], &response)
 	return response
