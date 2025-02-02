433396d35507da371d0d0d4d11d705c9e7429a1a
lukluk santoso <lukluk.santoso@bankmas.co.id>
2025-02-02 12:15:49 +0700

    fix issue with find path

--- scheduler/plugin.go
+++ scheduler/plugin.go
@@ -28,9 +28,6 @@ func (j *Job) setupPlugin() {
 			continue
 		}
 		env["PLUGIN_NAME"] = plugin.Name
-		if _, err := os.Stat("/tmp/" + plugin.Name + ".sock"); err == nil {
-			continue
-		}
 		cmd := exec.Command(basePath + plugin.TypePlugin)
 		for key, value := range env {
 			cmd.Env = append(cmd.Env, key+"="+value)
