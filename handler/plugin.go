fe72149e55083200f9ccaba0e063b7b9528db2e0
lukluk santoso <lukluk.santoso@bankmas.co.id>
2025-02-03 21:51:10 +0700

    restart

--- handler/plugin.go
+++ handler/plugin.go
@@ -2,9 +2,11 @@ package handler
 
 import (
 	"encoding/json"
+	"fmt"
 	"net"
 	"net/http"
 	"os"
+	"os/exec"
 	"strconv"
 	"time"
 
@@ -19,8 +21,8 @@ func (h *Handler) executePlugins(plugins []string, request dto.SidraRequest, ctx
 		if plugin == "" {
 			continue
 		}
-
-		response = callPlugin(plugin, request)
+		dataPlugin := h.dataSet.Plugins[plugin]
+		response = callPlugin(plugin, request, dataPlugin)
 
 		if response.StatusCode != 0 && response.StatusCode != http.StatusOK {
 			h.httpStatusCounter.WithLabelValues(strconv.Itoa(response.StatusCode), request.Url, string(ctx.Host()), dataplane, gs).Inc()
@@ -42,11 +44,12 @@ func (h *Handler) executePlugins(plugins []string, request dto.SidraRequest, ctx
 	return response
 }
 
-func callPlugin(pluginName string, request dto.SidraRequest) (response dto.SidraResponse) {
+func callPlugin(pluginName string, request dto.SidraRequest, dataPlugin dto.Plugin) (response dto.SidraResponse) {
 	//@TODO: for now body ignored
 	request.Body = ""
 	conn, err := net.Dial("unix", "/tmp/"+pluginName+".sock")
 	if err != nil {
+		restartPlugin(dataPlugin)
 		return dto.SidraResponse{
 			StatusCode: http.StatusInternalServerError,
 			Body:       "Failed to connect to plugin: " + pluginName,
@@ -65,9 +68,10 @@ func callPlugin(pluginName string, request dto.SidraRequest) (response dto.Sidra
 	return response
 }
 
-func callPluginWithBody(pluginName string, request dto.SidraRequest) (response dto.SidraResponse) {
+func callPluginWithBody(pluginName string, request dto.SidraRequest, dataPlugin dto.Plugin) (response dto.SidraResponse) {
 	conn, err := net.Dial("unix", "/tmp/"+pluginName+".sock")
 	if err != nil {
+		restartPlugin(dataPlugin)
 		return dto.SidraResponse{
 			StatusCode: http.StatusInternalServerError,
 			Body:       "Failed to connect to plugin: " + pluginName,
@@ -84,4 +88,36 @@ func callPluginWithBody(pluginName string, request dto.SidraRequest) (response d
 	n, _ := conn.Read(buffer[0:])
 	json.Unmarshal(buffer[:n], &response)
 	return response
+}
+
+func restartPlugin(plugin dto.Plugin) {
+	if plugin.Enabled == 0 {
+		return
+	}
+	basePath := "/usr/local/bin/"
+	if _, err := os.Stat("/usr/local/bin/" + plugin.TypePlugin); err != nil {
+		if _, err := os.Stat("./plugins/plugin-" + plugin.TypePlugin + "/" + plugin.TypePlugin); err != nil {
+			fmt.Println("Plugin does not exist")
+			return
+		} else {
+			basePath = "./plugins/plugin-" + plugin.TypePlugin + "/"
+		}
+	}
+	os.Remove("/tmp/" + plugin.Name + ".sock")
+	env := make(map[string]string)
+	err := json.Unmarshal([]byte(plugin.Config), &env)
+	if err != nil {
+		fmt.Println("- Failed to parse plugin config", plugin.TypePlugin, plugin.Config, err)
+		return
+	}
+	env["PLUGIN_NAME"] = plugin.Name
+	cmd := exec.Command(basePath + plugin.TypePlugin)
+	for key, value := range env {
+		cmd.Env = append(cmd.Env, key+"="+value)
+	}
+	err = cmd.Start()
+	if err != nil {
+		fmt.Println("- Failed to start plugin", plugin.TypePlugin, err)
+		return
+	}
 }
